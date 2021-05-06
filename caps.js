19bd7bd3c386c0ea8c80af4eecf5822fa7d7f407
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-05-05 17:08:01 -0700

    refactor

--- caps.js
+++ caps.js
@@ -1,42 +1,48 @@
 'use strict';
 
 require('dotenv').config();
-const port = process.env.PORT || 3000;
-const io = require('socket.io')(port);
+const PORT = process.env.PORT || 3000;
+const io = require('socket.io')(PORT);
 
 const caps = io.of('/caps');
 
 // -------------- ORDER --------------
-io.on('connection', (socket) => {
-  console.log('CONNECTED', socket.id);
+io.on('connection', socket => {
+  console.log('CONNECTED! User namespace:', socket.id);
 });
 
-caps.on('connection', (socket) => {
-  socket.on('CapOrder', payload => {
+caps.on('connection', socket => {
+
+  socket.on('join', room => {
+    console.log('room name:', room);
+    socket.join(room);
+  });
+
+  socket.on('pickup', payload => {
     console.log('EVENT:', {
       event: 'pickup',
       time: new Date(),
       payload: payload
     });
-    caps.emit('DriverPickup', payload);
+    caps.emit('pickup', payload);
   });
 
-  socket.on('CapInTransit', payload => {
+  socket.on('in-transit', payload => {
     console.log('EVENT:', {
       event: 'in-transit',
       time: new Date(),
       payload: payload
     });
-    caps.emit('DriverInTransit', payload);
+    caps.to(payload.store).emit('in-transit', payload);
   });
 
-  socket.on('CapDelivered', payload => {
-    caps.emit('VendorDelivered', payload);
+  socket.on('delivered', payload => {
     console.log('EVENT:', {
       event: 'delievered',
       time: new Date(),
       payload: payload
     });
+    caps.to(payload.store).emit('delivered', payload);
   });
 
 });
--- events.js
+++ events.js
@@ -1,8 +1,30 @@
 'use strict';
 
-const port = process.env.PORT || 3000;
-const io = require('socket.io')(port);
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const faker = require('faker');
+const io = require('socket.io-client');
+const HOST = process.env.HOST || 'http://localhost:3000';
+const PORT2 = process.env.PORT2 || 3001;
+const socket = io.connect(`${HOST}/caps`);
+// const io = require('socket.io')(PORT2);
 
-require('./caps.js')(io);
-require('./customers/drivers/driver')(io);
-require('./customers/vendors/vendor.js')(io);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/pickup', (req, res) => {
+  let fakeOrder = req.body || {
+    store: process.env.SHOP || '1-800-flowerz',
+    orderId: faker.datatype.uuid(),
+    customerName: faker.name.findName(),
+    address: faker.address.streetAddress()
+  };
+  socket.emit('pickup', fakeOrder);
+  res.status(200).send('Your package was scheduled');
+});
+
+app.listen(PORT2, () =>{
+  console.log(`API server is up on ${PORT2}`);
+});
